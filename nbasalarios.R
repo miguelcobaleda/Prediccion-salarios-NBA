013e20b116cb14db04be088b54115a41c1e619c4
Miguel <coba@usal.es>
2020-10-28 02:15:06 +0100

    'salarios'

---
+++ nbasalarios.R
@@ -0,0 +1,34 @@
+################## PREDICCION SALARIOS NBA ###################
+
+library(readr)
+nba <- read.csv('./nba.csv')
+
+# Creacion modelo lineal
+mod1 <- lm(Salary ~ NBA_Country + NBA_DraftNumber + 
+             Age + Tm + G + MP + PER + TS. + X3PAr +
+             FTr + ORB. + DRB. + TRB. + AST. + STL. +
+             BLK. + TOV. + USG. + OWS + DWS + WS + WS.48 +
+             OBPM + DBPM + BPM + VORP, data=nba)
+head(nba, 5)
+summary(mod1)
+
+# Seleccion de un nuevo modelo mediante criterio AIC
+library(MASS)
+stepAIC(object = mod1, direction = "both")
+
+# Creacion del nuevo modelo lineal
+mod2 <- lm(Salary ~ NBA_DraftNumber + Age + G + MP + PER + 
+             X3PAr + ORB. + TRB. + USG. + WS + OBPM, data=nba)
+summary(mod2)
+
+# Predicción
+nba$SalaryPredict <- predict(mod2, newdata=nba)
+
+# Validacion
+mod3 <- lm(Salary ~ NBA_DraftNumber + Age + G + MP + PER + 
+             X3PAr + ORB. + TRB. + USG. + WS + OBPM, data=nba[1:339,])
+
+nba$Validation <- predict(mod3, newdata=nba)
+
+nba <- na.omit(nba)
+mean(1/nrow(nba)*((nba$SalaryPredict/nba&Validation)^2))
